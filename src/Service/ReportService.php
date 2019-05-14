6b93a08c57bfaad59082b908dbae0a3974e95466
Jakub Kuryłowicz <jakub.kurylowicz@gnb.pl>
2019-05-14 14:45:20 +0100

    small changes

--- src/Service/ReportService.php
+++ src/Service/ReportService.php
@@ -27,29 +27,37 @@ class ReportService
 
         foreach ($profits as $profit) {
             $Ym = $profit->getIssueDate()->format('Y-m');
+            if (!isset($reports[$Ym])) {
+                $reports[$Ym]['profits_net'] = 0;
+                $reports[$Ym]['profits_tax'] = 0;
+                $reports[$Ym]['profits_gross'] = 0;
+                $reports[$Ym]['costs_net'] = 0;
+                $reports[$Ym]['costs_tax'] = 0;
+                $reports[$Ym]['costs_gross'] = 0;
+            }
             $reports[$Ym]['fullMonth'] = $profit->getIssueDate()->format('F, Y');
             $reports[$Ym]['profits'][] = $profit;
 
-            if (!isset($reports[$Ym]['profits_net'])) {
-                $reports[$Ym]['profits_net'] = 0;
-                $reports[$Ym]['profits_gross'] = 0;
-            }
-
             $reports[$Ym]['profits_net'] += $profit->getNet();
+            $reports[$Ym]['profits_tax'] += $profit->getTax();
             $reports[$Ym]['profits_gross'] += $profit->getGross();
         }
 
         foreach ($costs as $cost) {
             $Ym = $cost->getIssueDate()->format('Y-m');
+            if (!isset($reports[$Ym])) {
+                $reports[$Ym]['profits_net'] = 0;
+                $reports[$Ym]['profits_tax'] = 0;
+                $reports[$Ym]['profits_gross'] = 0;
+                $reports[$Ym]['costs_net'] = 0;
+                $reports[$Ym]['costs_tax'] = 0;
+                $reports[$Ym]['costs_gross'] = 0;
+            }
             $reports[$Ym]['fullMonth'] = $cost->getIssueDate()->format('F, Y');
             $reports[$Ym]['costs'][] = $cost;
 
-            if (!isset($reports[$Ym]['costs_net'])) {
-                $reports[$Ym]['costs_net'] = 0;
-                $reports[$Ym]['costs_gross'] = 0;
-            }
-
             $reports[$Ym]['costs_net'] += $cost->getNet();
+            $reports[$Ym]['costs_tax'] += $cost->getTax();
             $reports[$Ym]['costs_gross'] += $cost->getGross();
         }
 
