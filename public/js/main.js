0ff35b9809bb5c210d2282447b740cd72a31f4f8
Jakub Kuryłowicz <jakub.kurylowicz@gnb.pl>
2019-06-18 13:54:13 +0100

    fullscreen

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
     let hammertime = new Hammer(myElement);
     // hammertime.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
-    hammertime.on('swiperight swipeleft', function(event) {
+    hammertime.on('swiperight swipeleft pan', function(event) {
 
         if (event.type == 'swiperight') {
             $('.content > .wrapper').addClass('collapsed');
@@ -23,6 +23,23 @@ $(document).ready(function () {
         if (event.type == 'swipeleft') {
             $('.content > .wrapper').removeClass('collapsed');
             $('.menu').removeClass('expanded');
+        }
+
+        if (event.type == 'panup') {
+
+            if (event.changedPointers.length != 2) {
+                return;
+            }
+
+            elem = document.querySelector('#body');
+
+            if(elem.requestFullScreen) {
+                elem.requestFullScreen();
+            } else if(elem.mozRequestFullScreen) {
+                elem.mozRequestFullScreen();
+            } else if(elem.webkitRequestFullScreen) {
+                elem.webkitRequestFullScreen();
+            }
         }
     });
 
